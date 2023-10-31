6f627533e12c294ac5b591e4a44b94ec6b5a133c
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-10-31 19:18:40 +0300

    create project

--- src/com/TourismAgencySystem/View/EmployeeGUI.java
+++ src/com/TourismAgencySystem/View/EmployeeGUI.java
@@ -15,12 +15,30 @@ public class EmployeeGUI extends JFrame{
     private JTabbedPane tabbedPane4;
     private JTable table1;
     private JTable table2;
+    private JButton buttonHotelDelete;
+    private JButton buttonHotelSelect;
+    private JTable tableHotelList;
+    private JTextField fieldHotelName;
+    private JTextField fieldHotelCity;
+    private JTextField fieldHotelDistrict;
+    private JTextField fieldHotelStar;
+    private JTextField fieldHotelAddress;
+    private JTextField fieldHotelMail;
+    private JTextField fieldHotelPhone;
+    private JCheckBox checkBoxParking;
+    private JCheckBox wiFiCheckBox;
+    private JCheckBox checkBoxPool;
+    private JCheckBox checkBoxGym;
+    private JCheckBox checkBoxSpa;
+    private JCheckBox checkBoxRoomService;
+    private JCheckBox checkBoxConcierge;
+    private JScrollPane checkBoxWifi;
     private Employee employee;
 
     public EmployeeGUI(Employee employee) {
         this.employee = employee;
         add(wrapper);
-        setSize(1000, 500);
+        setSize(1000, 700);
         setLocation(Helper.screenCenterLocation("x", getSize()), Helper.screenCenterLocation("y", getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
