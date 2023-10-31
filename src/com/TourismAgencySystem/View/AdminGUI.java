f7c9984d9276a2dbe4ed23186659e08c513201d0
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-10-31 14:10:27 +0300

    create project

---
+++ src/com/TourismAgencySystem/View/AdminGUI.java
@@ -0,0 +1,24 @@
+package com.TourismAgencySystem.View;
+
+import com.TourismAgencySystem.Helper.Config;
+import com.TourismAgencySystem.Helper.Helper;
+import com.TourismAgencySystem.Model.Admin;
+
+import javax.swing.*;
+
+public class AdminGUI extends JFrame{
+    private JPanel wrapper;
+    private final Admin admin;
+
+    public AdminGUI (Admin admin) {
+        this.admin = admin;
+        add(wrapper);
+        setSize(1000, 500);
+        int x = Helper.screenCenterLocation("x", getSize());
+        int y = Helper.screenCenterLocation("y", getSize());
+        setLocation(x, y);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle(Config.PROJECT_TITLE);
+        setVisible(true);
+    }
+}
