985146e84261628fc0b9f3cbd3665a29ac269ceb
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-11-01 16:02:01 +0300

    GUIs

--- src/com/TourismAgencySystem/View/EmployeeGUI.java
+++ src/com/TourismAgencySystem/View/EmployeeGUI.java
@@ -46,14 +46,43 @@ public class EmployeeGUI extends JFrame {
     private JPanel roomPanelTop;
     private JComboBox comboBoxRoomType;
     private JButton buttonRoomTypeSelect;
-    private JButton button1;
-    private JButton button2;
+    private JButton buttonRoomTypeUpdate;
+    private JButton buttonRoomTypeAdd;
     private JTextField fieldRoomTypeName;
     private JTextField fieldRoomStock;
     private JTextField fieldRoomBedCount;
     private JTextField fieldRoomSize;
     private JTextField fieldRoomTv;
     private JTextField fieldRoomMinibar;
+    private JComboBox comboBoxPeriodSingle;
+    private JComboBox comboBoxPeriodDouble;
+    private JComboBox comboBoxPeriodKingSuite;
+    private JComboBox comboBoxGuestTypeSingle;
+    private JComboBox comboBoxGuestTypeDouble;
+    private JComboBox comboBoxGuestTypeKingSuite;
+    private JComboBox comboBoxHostelTypeSingle;
+    private JComboBox comboBoxHostelTypeDouble;
+    private JComboBox comboBoxHostelTypeKingSuite;
+    private JTextField fieldPriceSingle;
+    private JTextField fieldPriceDouble;
+    private JTextField fieldPriceKingSuite;
+    private JButton buttonAddSingle;
+    private JButton buttonAddDouble;
+    private JButton buttonAddKingSuite;
+    private JRadioButton radioButtonSingle;
+    private JRadioButton radioButtonDouble;
+    private JRadioButton radioButtonKingSuite;
+    private JButton buttonDeletePrice;
+    private JTable tableRoomPriceList;
+    private JTable tableSearchList;
+    private JButton buttonSearchSelect;
+    private JButton buttonSearchSearch;
+    private JTextField fieldSearchHotelCityDistrict;
+    private JTextField fieldSearchStar;
+    private JTextField fieldSearchCheckin;
+    private JTextField fieldSearchCheckout;
+    private JTextField fieldSearchAdult;
+    private JTextField fieldSearchChild;
     private Employee employee;
 
     public EmployeeGUI(Employee employee) {
@@ -99,6 +128,75 @@ public class EmployeeGUI extends JFrame {
                     fieldOffSeasonEndDate.setEditable(false);
                     fieldOffSeasonEndDate.setText(null);
                     fieldOffSeasonStartDate.setText(null);
+                }
+            }
+        });
+        radioButtonSingle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comboBoxPeriodSingle.setEnabled(true);
+                comboBoxGuestTypeSingle.setEnabled(true);
+                comboBoxHostelTypeSingle.setEnabled(true);
+                fieldPriceSingle.setEnabled(true);
+                fieldPriceSingle.setEditable(true);
+                buttonAddSingle.setEnabled(true);
+                if (!radioButtonSingle.isSelected()) {
+                    comboBoxPeriodSingle.setEnabled(false);
+                    comboBoxPeriodSingle.setEditable(false);
+                    comboBoxGuestTypeSingle.setEnabled(false);
+                    comboBoxGuestTypeSingle.setEditable(false);
+                    comboBoxHostelTypeSingle.setEnabled(false);
+                    comboBoxHostelTypeSingle.setEditable(false);
+                    fieldPriceSingle.setEnabled(false);
+                    fieldPriceSingle.setEditable(false);
+                    fieldPriceSingle.setText(null);
+                    buttonAddSingle.setEnabled(false);
+                }
+            }
+        });
+        radioButtonDouble.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comboBoxPeriodDouble.setEnabled(true);
+                comboBoxGuestTypeDouble.setEnabled(true);
+                comboBoxHostelTypeDouble.setEnabled(true);
+                fieldPriceDouble.setEnabled(true);
+                fieldPriceDouble.setEditable(true);
+                buttonAddDouble.setEnabled(true);
+                if (!radioButtonDouble.isSelected()) {
+                    comboBoxPeriodDouble.setEnabled(false);
+                    comboBoxPeriodDouble.setEditable(false);
+                    comboBoxGuestTypeDouble.setEnabled(false);
+                    comboBoxGuestTypeDouble.setEditable(false);
+                    comboBoxHostelTypeDouble.setEnabled(false);
+                    comboBoxHostelTypeDouble.setEditable(false);
+                    fieldPriceDouble.setEnabled(false);
+                    fieldPriceDouble.setEditable(false);
+                    fieldPriceDouble.setText(null);
+                    buttonAddDouble.setEnabled(false);
+                }
+            }
+        });
+        radioButtonKingSuite.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comboBoxPeriodKingSuite.setEnabled(true);
+                comboBoxGuestTypeKingSuite.setEnabled(true);
+                comboBoxHostelTypeKingSuite.setEnabled(true);
+                fieldPriceKingSuite.setEnabled(true);
+                fieldPriceKingSuite.setEditable(true);
+                buttonAddKingSuite.setEnabled(true);
+                if (!radioButtonKingSuite.isSelected()) {
+                    comboBoxPeriodKingSuite.setEnabled(false);
+                    comboBoxPeriodKingSuite.setEditable(false);
+                    comboBoxGuestTypeKingSuite.setEnabled(false);
+                    comboBoxGuestTypeKingSuite.setEditable(false);
+                    comboBoxHostelTypeKingSuite.setEnabled(false);
+                    comboBoxHostelTypeKingSuite.setEditable(false);
+                    fieldPriceKingSuite.setEnabled(false);
+                    fieldPriceKingSuite.setEditable(false);
+                    fieldPriceKingSuite.setText(null);
+                    buttonAddKingSuite.setEnabled(false);
                 }
             }
         });
